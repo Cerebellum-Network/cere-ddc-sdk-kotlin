a620bddf57fea74b680f45d469dc43b194d018e9
upalinski <ulad@cere.io>
2022-02-14 23:59:45 +0300

    compile protoc via plugin

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -1,7 +1,15 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     id("com.google.protobuf") version Versions.protobufPlugin
 }
 
 dependencies {
     implementation("com.google.protobuf:protobuf-java:${Versions.protobuf}")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.6.1"
+    }
 }
