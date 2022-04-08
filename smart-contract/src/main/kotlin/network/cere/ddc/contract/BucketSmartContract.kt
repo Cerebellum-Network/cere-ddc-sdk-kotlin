d6e4a51c7a1919b1e0026d063e147b4140039700
svalasovich <svalasovich@gmail.com>
2022-04-08 17:11:07 +0400

    added Admin, Billing, Permission commands

--- smart-contract/src/main/kotlin/network/cere/ddc/contract/BucketSmartContract.kt
+++ smart-contract/src/main/kotlin/network/cere/ddc/contract/BucketSmartContract.kt
@@ -2,21 +2,20 @@ package network.cere.ddc.contract
 
 import network.cere.ddc.contract.blockchain.BlockchainConfig
 import network.cere.ddc.contract.blockchain.client.SmartContractClient
-import network.cere.ddc.contract.query.commander.BucketCommander
-import network.cere.ddc.contract.query.commander.ClusterCommander
-import network.cere.ddc.contract.query.commander.NodeCommander
-import network.cere.ddc.contract.query.service.BucketService
-import network.cere.ddc.contract.query.service.ClusterService
-import network.cere.ddc.contract.query.service.NodeService
+import network.cere.ddc.contract.query.commander.*
+import network.cere.ddc.contract.query.service.*
 import java.net.ConnectException
 
-//ToDo make dynamic EventReader and request buider by parsing JSON ABI
+//ToDo make dynamic EventReader and request builder by parsing JSON ABI
 class BucketSmartContract(
     client: SmartContractClient,
     bucketContractConfig: BucketContractConfig,
 ) : BucketCommander by BucketService(client, bucketContractConfig),
     ClusterCommander by ClusterService(client, bucketContractConfig),
     NodeCommander by NodeService(client, bucketContractConfig),
+    AdminCommander by AdminService(client, bucketContractConfig),
+    PermissionCommander by PermissionService(client, bucketContractConfig),
+    BillingCommander by BillingService(client, bucketContractConfig),
     AutoCloseable by client {
 
     companion object {
