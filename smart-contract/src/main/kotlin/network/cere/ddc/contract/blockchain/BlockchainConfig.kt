63b8450e6b5de764ce76bf33281deb3fdf068a98
svalasovich <svalasovich@gmail.com>
2022-05-24 17:13:59 +0400

    changed privateKey to seed in smart-contract module

--- smart-contract/src/main/kotlin/network/cere/ddc/contract/blockchain/BlockchainConfig.kt
+++ smart-contract/src/main/kotlin/network/cere/ddc/contract/blockchain/BlockchainConfig.kt
@@ -6,7 +6,7 @@ import java.time.Duration
 data class BlockchainConfig(
     val wsUrl: String,
     val contractAddressHex: String,
-    val privateKeyHex: String,
+    val seedHex: String,
     val timeout: Duration = Duration.ofMinutes(1),
     val typeFiles: List<Path> = listOf()
 )
