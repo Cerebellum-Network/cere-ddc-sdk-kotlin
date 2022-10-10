4dd1f217169c878e5c98c021d572f0c5302ad8bc
isqander <isqander@gmail.com>
2022-10-10 18:28:27 +0400

    DDC-46 debug CI

--- protoSchemasPullCopy.sh
+++ protoSchemasPullCopy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "script start"
 git submodule add git@github.com:Cerebellum-Network/ddc-schemas.git
 git submodule init
 git submodule update
 
 mkdir proto/src/main/proto
 cp ddc-schemas/storage/protobuf/* proto/src/main/proto
+echo "script end"
